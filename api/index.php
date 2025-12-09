a1d6988c0a4f2545396e8dfb40fc73d9c8da0b62
ignatius christian <christian030280@gmail.com>
2025-12-10 01:08:12 +0700

    fix

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 chdir(__DIR__ . '/../');
-require __DIR__ . '/../public/index.php';
+
+// load composer autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// bootstrap laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
