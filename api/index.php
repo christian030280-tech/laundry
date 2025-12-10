e9f76e30ae1f70beeb149914d862f7ef180be90b
ignatius christian <christian030280@gmail.com>
2025-12-10 20:47:56 +0700

    SCA LAUNDRY

--- api/index.php
+++ api/index.php
@@ -1,18 +1,3 @@
 <?php
 chdir(__DIR__ . '/../');
-
-// load composer autoload
-require __DIR__ . '/../vendor/autoload.php';
-
-// bootstrap laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-
-$response = $kernel->handle($request);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
